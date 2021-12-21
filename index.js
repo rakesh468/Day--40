99d467f131feb524f2ee7fc5d8d120e0b17fece0
rakesh468 <rakeshkumarg2000@gmail.com>
2021-12-20 22:51:34 -0800

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 import express from "express";
 import fs from "fs";
+
+
+// creating a server using express //
 const app=express();
 
+// running server under PORT=>5500 //
 const PORT=5500;
 
 app.get("/",(request,response)=>{
 response.send("Hello World")
 })
+
 app.listen(PORT,()=>console.log("App is running in ",PORT))
 
+// creating a  data timestamp ///
 let data=`${new Date()}`;
 
 let file=`${new Date().getUTCDate}`
 
+// creating file in  new folder ///
 for(let i=0;i<10;i++){
     fs.writeFile(`./files/${file}.txt`,data,(err)=>{
         console.log("completed")
     })
 }
+
+// reading data in tht particullar folder //
 fs.readdir("./files",(err,files)=>{
     console.log(files)
 })
 
+// read data on the created folder//
 for(let i=0; i<10; i++){
     fs.readFile(`./${file}.txt`,"utf-8",(err)=>{
 console.log(data);
